594a5f2f8749b84fcafaa2c7e8ee15f3e27a30c2
strawgate <williamseaston@gmail.com>
2016-06-01 10:18:31 -0500

    Escape percentage symbol from file name

--- Helpers/Applocker/Applocker.psm1
+++ Helpers/Applocker/Applocker.psm1
@@ -17,9 +17,10 @@ A string you wish to escape
       ValueFromPipelineByPropertyName=$true)]
     [String]$Name
   )
-
+  
   $invalidChars = [IO.Path]::GetInvalidFileNameChars() -join ''
-  $re = "[{0}]" -f [RegEx]::Escape($invalidChars)
+  $re = "[{0}%]" -f [RegEx]::Escape($invalidChars)
+
   return ($Name -replace $re)
 }
 
